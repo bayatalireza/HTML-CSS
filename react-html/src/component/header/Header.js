c9824ad7b82faa8053f650c6fddb8bfeab48730c
alireza <bayatalireza86@gmail.com>
2024-10-17 18:35:15 +0330

    added header files

--- react-html/src/component/header/Header.js
+++ react-html/src/component/header/Header.js
@@ -5,14 +5,15 @@ const Header = () => {
     <div>
       <header className={styles.header}>
         <div className={styles.banner}>
-          <h1>.مجموعه جملات و متن های زیبا از بزرگان</h1>
+          <h1>مجموعه جملات زیبا از بزرگان</h1>
           <h2>
             برای اجتناب از بحران می توانید از سیاست سکوت استفاده کنید:سوزان
             کوئیلیام
           </h2>
-          <a href="../products/Products">محصولات</a>
+          <a className={styles.btn} href="../products/Products">محصولات</a>
         </div>
       </header>
+      <div className={styles.contentDivider}></div>
     </div>
   );
 };
